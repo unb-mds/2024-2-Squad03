d8ed5beace24c8428e3fa65194ca5587d0a5220d
Potatoyz908 <euller2005@gmail.com>
2025-01-31 22:18:37 -0300

    feat(asgi): Adicionando suporte a múltiplas rotas WebSocket e atualizando dependências

--- API/AcheiUnB/asgi.py
+++ API/AcheiUnB/asgi.py
@@ -4,17 +4,19 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from chat.routing import websocket_urlpatterns
+from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
+from users.routing import websocket_urlpatterns as users_websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AcheiUnB.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                chat_websocket_urlpatterns
+                + users_websocket_urlpatterns  # ✅ Adiciona múltiplas rotas WebSocket
+            )
+        ),
     }
 )
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AcheiUnB.settings")
-
-application = get_asgi_application()
